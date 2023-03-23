bfbea83451f7d7b18e4531c1386eb42468b90392
khardy <khardy.official@gmail.com>
2023-03-23 18:34:38 +0200

    HW5 final project init

---
+++ final/src/App/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>sadasdasd</h1>
+      <button className='btn'>dfgdfg</button>
+    </div>
+  );
+}
+
+export default App;
