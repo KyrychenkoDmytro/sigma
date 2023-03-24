b78707356df89a6301015c0823e10a939fadde27
khardy <khardy.official@gmail.com>
2023-03-24 13:19:01 +0200

    OfferBanner section

--- final/src/pages/Home/Home.jsx
+++ final/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import './Home.scss';
 import Banner from '../../Components/Banner/Banner';
+import OfferBanner from '../../Components/OfferBanner/OfferBanner';
 
 const Home = () => {
     return (
         <main className="Home">
             <Banner />
+            <OfferBanner />
         </main>
     );
 }
