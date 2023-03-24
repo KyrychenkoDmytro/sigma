4190c9c36c3eb4e1bee79e294854528e5aae6213
khardy <khardy.official@gmail.com>
2023-03-24 10:37:48 +0200

    Banner section

--- final/src/App/App.js
+++ final/src/App/App.js
@@ -1,14 +1,17 @@
 import './App.scss';
 import Header from '../Containers/Header/Header';
+import Home from '../pages/Home/Home';
 import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Routes>
-        <Route path='#'/>
-      </Routes>
+      <div className="wrapper">
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
---
+++ final/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.scss';
+import Banner from '../../Components/Banner/Banner';
+
+const Home = () => {
+    return (
+        <main className="Home">
+            <Banner />
+        </main>
+    );
+}
+
+export default Home;
