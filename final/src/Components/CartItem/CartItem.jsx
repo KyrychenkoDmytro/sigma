b091acb36b744920d956a85c2c2af4eae1869c77
khardy <khardy.official@gmail.com>
2023-03-28 16:29:39 +0300

    CartForm add

--- final/src/Components/CartItem/CartItem.jsx
+++ final/src/Components/CartItem/CartItem.jsx
@@ -10,7 +10,7 @@ const CartItem = () => {
                     <h3 className="title CartItem__title">Calabrese Broccoli</h3>
                     <div className="CartItem__old-price">$20.00</div>
                     <div className="CartItem__new-price">$13.00</div>
-                    <div className="CartItem__quantity-label">Quantity: </div>
+                    <div className="CartItem__quantity-label">Quantity : </div>
                     <div className="CartItem__quantity">2</div>
                     <button className="CartItem__btn-cancel-order"></button>
                 </div>
--- final/src/Containers/CartOrder/CartOrder.jsx
+++ final/src/Containers/CartOrder/CartOrder.jsx
@@ -1,5 +1,6 @@
 import './CartOrder.scss';
-import CartItem from '../CartItem/CartItem';
+import CartItem from '../../Components/CartItem/CartItem';
+import CartForm from '../CartForm/CartForm';
 
 const CartOrder = () => {
 
@@ -8,6 +9,10 @@ const CartOrder = () => {
             <div className="container">
                 <div className="CartOrder__wrap">
                    <CartItem />
+                   <h3 className="title CartOrder__total-cost"><pre>Total Cost  <span>26$</span></pre></h3>
+                   <h3 className="title CartOrder__total-discont"><pre>Discount    <span>14$</span></pre></h3>
+                   <button className="btn btn_gray CartOrder__btn-order" id="btn-cart-to-order">To order</button>
+                   <CartForm />
                 </div>
             </div>
         </div>
